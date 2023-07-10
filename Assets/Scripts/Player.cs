6fd15934cdb8c3e732624332efabe76ddbd839ef
Burak Ekemen <bekemen98@gmail.com>
2023-07-10 23:09:48 +0300

    bullet hit and exp

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject bulletPrefab;
+
+    Camera cam;
+    public float width;
+    private float speed = 3f;
+
+    bool isShooting;
+    float coolDown = 0.5f;
+    [SerializeField] private ObjectPool objectPool = null;
+
+    private void Awake()
     {
-        
+        cam = Camera.main;
+        width = ((1 / (cam.WorldToViewportPoint(new Vector3(1, 1, 0)).x - .5f) / 2) - 0.25f);
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+
+    }
+
+
     void Update()
     {
-        
+#if UNITY_EDITOR
+        if (Input.GetKey(KeyCode.A) && transform.position.x > -width)
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D) && transform.position.x < width)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.Space) && !isShooting)
+        {
+            StartCoroutine(Shoot());
+        }
+#endif
     }
+
+    private IEnumerator Shoot()
+    {
+        isShooting = true;
+
+        //Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        GameObject obj = objectPool.GetPooledObject();
+        obj.transform.position = gameObject.transform.position;
+        yield return new WaitForSeconds(coolDown);
+
+        isShooting = false;
+    }
+
 }
